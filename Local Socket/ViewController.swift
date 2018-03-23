1d03a7d3278a958db8c0908517d6fe48d8b93cac
Shockki <tolimodest@gmail.com>
2018-03-23 17:09:24 +0300

    Initial Commit

---
+++ Local Socket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Local Socket
+//
+//  Created by Анатолий on 23.03.2018.
+//  Copyright © 2018 Анатолий Модестов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
